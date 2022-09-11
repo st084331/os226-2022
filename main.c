2b09312f6eed579c65cf5c43b7d231cbe75debef
st084331 <71183199+st084331@users.noreply.github.com>
2022-09-11 13:32:45 +0300

    retcode and echo v2 with "<"

--- main.c
+++ main.c
@@ -19,43 +19,31 @@ int retcode(int argc, char *argv[]) {
 int main(int argc, char *argv[]) {
     char retcodeCommand[8] = "retcode";
     char echoCommand[5] = "echo";
-    for (int i = 1; i < argc; ++i) {
-        FILE *fp;
-        fp = fopen(argv[i], "r");
-        if (fp == NULL) {
-            retcodeInt = -1;
-        } else {
-            char *line = NULL;
-            size_t len = 0;
-            while (getline(&line, &len, fp) != -1) {
-                char *pch;
-                char *commands[1024];
-                int commandsCount = 0;
-                for (pch = strtok(line, ";\n"); pch && commandsCount < 1024; pch = strtok(NULL, ";\n")) {
-                    commands[commandsCount++] = pch;
-                }
-                for (int i = 0; i < commandsCount; i++) {
-                    char *ptr;
-                    char *oneLineCommand[1024];
-                    int count = 0;
-                    for (ptr = strtok(commands[i], " \n"); ptr && count < 1024; ptr = strtok(NULL, " \n")) {
-                        oneLineCommand[count++] = ptr;
-                    }
-                    if (count > 0) {
-                        if (strcmp(echoCommand, oneLineCommand[0]) == 0) {
-                            retcodeInt = echo(count, oneLineCommand);
-                        }
-                        if (strcmp(retcodeCommand, oneLineCommand[0]) == 0) {
-                            retcodeInt = retcode(count, oneLineCommand);
-                        }
-                    } else {
-                        retcodeInt = 0;
-                    }
-                }
+    char input_buffer[32768];
+    while(fgets(input_buffer, 32768, stdin) != NULL) {
+        char *pch;
+        char *commands[1024];
+        int commandsCount = 0;
+        for (pch = strtok(input_buffer, ";\n"); pch && commandsCount < 1024; pch = strtok(NULL, ";\n")) {
+            commands[commandsCount++] = pch;
+        }
+        for (int j = 0; j < commandsCount; j++) {
+            char *ptr;
+            char *oneLineCommand[1024];
+            int count = 0;
+            for (ptr = strtok(commands[j], " \n"); ptr && count < 1024; ptr = strtok(NULL, " \n")) {
+                oneLineCommand[count++] = ptr;
             }
-            fclose(fp);
-            if (line)
-                free(line);
+            if (count > 0) {
+                if (strcmp(echoCommand, oneLineCommand[0]) == 0) {
+                    retcodeInt = echo(count, oneLineCommand);
+                }
+                if (strcmp(retcodeCommand, oneLineCommand[0]) == 0) {
+                    retcodeInt = retcode(count, oneLineCommand);
+                }
+            } else {
+                retcodeInt = 0;
+            }
         }
     }
     return 0;
